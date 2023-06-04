7fc7e0234094b200057aa2cda443b24e6c72a068
David Turner <csusbdt@gmail.com>
2023-06-04 11:00:34 -1000

    X

--- docs/away/index.js
+++ docs/away/index.js
@@ -3,11 +3,24 @@ import start_far_away from "./far_away/index.js";
 
 const draw_back_border      = image("./away/images/back_border.png");
 const draw_back_red         = image("./away/images/back_red.png");
-const draw_back_green       = image("./away/images/back_green.png");
+//const draw_back_green       = image("./away/images/back_green.png");
 
 const back_click = circle(125, 130,  65);
-let draw_back = null;
+//let draw_back = null;
 
+function c_song() {
+}
+
+c_song.prototype.stop = function() {
+    console.log("stop song");
+};
+
+c_song.prototype.start = function() {
+    console.log("start song");
+};
+
+const song = () => { return new c_song(); }
+    
 function c_button(name, shape, song) {
     this.border = image("./away/images/play_" + name + "_border.png");
     this.red    = image("./away/images/play_" + name + "_red.png"   );
@@ -23,8 +36,22 @@ c_button.prototype.click = function() {
     if (this.shape()) {
         if (proto.playing === this) {
             proto.playing = null;
-        } else {
+            this.red();
+            this.border();
+            this.song.stop();
+        } else if (proto.playing === null) {
+            this.song.start();
             proto.playing = this;            
+            this.green();
+            this.border();
+        } else {
+            proto.playing.song.stop(); // maybe not needed
+            proto.playing.red();
+            proto.playing.border();
+            proto.playing = this;            
+            this.green();
+            this.border();
+            this.song.start();
         }
     }
 };
@@ -40,33 +67,39 @@ c_button.prototype.draw = function() {
 };
 
 const buttons = [
-    new c_button("big"     , circle(460, 550, 170), {}),
-    new c_button("medium_1", circle(770, 330, 100), {}),
-    new c_button("medium_2", circle(760, 808, 115), {}),
-    new c_button("small_1" , circle(230, 393,  56), {}),
-    new c_button("small_2" , circle(166, 754,  40), {}),
-    new c_button("small_3" , circle(420, 812,  45), {}),
-    new c_button("small_4" , circle(780, 570,  45), {})
+    new c_button("big"     , circle(460, 550, 170), song()),
+    new c_button("medium_1", circle(770, 330, 100), song()),
+    new c_button("medium_2", circle(760, 808, 115), song()),
+    new c_button("small_1" , circle(230, 393,  56), song()),
+    new c_button("small_2" , circle(166, 754,  40), song()),
+    new c_button("small_3" , circle(420, 812,  45), song()),
+    new c_button("small_4" , circle(780, 570,  45), song())
 ];
 
 const click = _ => {
     if (back_click()) {
-        draw_back = loop([draw_back_green, draw_back_border], start_home);
+        start_home();
+//        draw_back = loop([draw_back_green, draw_back_border], start_home);
     } else {
         buttons.forEach(button => button.click());
     }
 };
 
-const draw = _ => {
-    draw_blue_bg();
-    buttons.forEach(button => button.draw());
-    draw_back();
-};
+// const draw = _ => {
+//     draw_blue_bg();
+//     buttons.forEach(button => button.draw());
+//     draw_back();
+// };
 
 const start = _ => {
-    draw_back = loop([draw_back_red, draw_back_border]);
+    draw_blue_bg();
+    buttons.forEach(button => button.draw());
+    draw_back_red();
+    draw_back_border();
+
+//    draw_back = loop([draw_back_red, draw_back_border]);
     set_click(click);
-    set_draw(350, draw);
+//    set_draw(350, draw);
 };
 
 export default start;
--- docs/home/index.js
+++ docs/home/index.js
@@ -10,32 +10,17 @@ const click = _ => {
     if (big_button_clicked()) {
         schedule(
             [  0, draw_blue_bg, big_button_1],
-            [350, draw_blue_bg, big_button_2],
-            [350, start_away                ]
+            [100, draw_blue_bg, big_button_2],
+            [100, start_away                ]
         );
-        // draw_blue_bg();
-        // big_button_1();
-        // setTimeout(_ => {
-        //     draw_blue_bg();
-        //     big_button_2();
-        //     setTimeout(start_away, 350);            
-        // }, 350);
-        
-//        big_button = once(big_button_1, big_button_2, [big_button_2, start_away]);
     }
 };
-
-// const draw = _ => {
-//     draw_blue_bg();
-//     big_button();
-// };
 
 const start = _ => {
     set_draw();
     draw_blue_bg();
     big_button_0();
     set_click(click);
-//    set_draw(350, draw);
 };
 
 export default start;
