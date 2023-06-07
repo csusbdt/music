1f816846c460d399d742eeb0ae991c8747cfc91f
David Turner <csusbdt@gmail.com>
2023-06-07 04:12:31 -1000

    X

--- docs/songs/index.js
+++ docs/songs/index.js
@@ -1,73 +1,11 @@
-import start_home     from "../home/index.js";
-import song           from "./song.js";
-
-const song_0 = song([	
-	[ 77, 0.50, 0.86],
-	[ 52, 0.34, 0.60],
-	[ 49, 0.62, 0.22],
-	[ 90, 0.44, 0.56],
-	[259, 0.31, 0.43],
-	[193, 0.67, 0.37],
-	[ 65, 0.68, 0.32],
-	[ 55, 0.45, 0.29],
-	[152, 0.32, 0.28],
-	[248, 0.26, 0.23],
-	[273, 0.24, 0.26],
-	[330, 0.23, 0.28],
-	[446, 0.23, 0.25],
-	[408, 0.38, 0.25],
-	[246, 0.52, 0.24],
-	[153, 0.46, 0.23],
-	[134, 0.34, 0.27],
-	[ 97, 0.30, 0.70]
-]);
-
-const song_1 = song([
-	[ 77, 0.50, 0.86],
-	[ 52, 0.34, 0.86],
-	[ 77, 0.62, 0.86],
-	[ 52, 0.44, 0.43],
-	[260, 0.31, 0.43],
-	[193, 0.67, 0.86],
-	[340, 0.41, 0.21],
-	[293, 0.31, 0.21],
-	[260, 0.41, 0.43],
-	[340, 0.65, 0.32],
-	[177, 0.68, 0.86],
-	[135, 0.45, 0.43],
-	[152, 0.32, 0.21],
-	[248, 0.26, 0.22]
-]);
-
-// const song_2 = [
-//     [170, .39, 1.00],
-// 	[ 47, .52, 1.00],
-// 	[ 65, .42, 1.00],
-// 	[ 81, .69, 1.00],
-// 	[239, .48, 1.00],
-// 	[ 96, .18, 1.00],
-// 	[ 51, .42, 1.00],
-// 	[ 69, .66, 1.00]
-// ];
-
-// const song_3 = [
-//     [127, 0.25, .59],
-//     [249, 0.52, .40],
-//     [335, 0.34, .40],
-//     [108, 0.41, .51],
-//     [142, 0.62, .22],
-//     [201, 0.59, .26],
-//     [172, 0.38, .40]
-// ];
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
+import start_home  from "../home/index.js";
+import { song_0 }  from "./songs.js"      ;
+import { song_1 }  from "./songs.js"      ;
+import { song_2 }  from "./songs.js"      ;
+import { song_3 }  from "./songs.js"      ;
 
 const back_border      = image("./songs/images/back_border.png");
 const back_red         = image("./songs/images/back_red.png");
-
 
 const song_0_button = radio_button(
 	image("./songs/images/play_big_border.png", 0, 0, 1                     ),
@@ -81,9 +19,23 @@ const song_1_button = radio_button(
 	image("./songs/images/play_medium_1_green.png" , 0, 0, 1, song_1.stop_func ())
 );
 
+const song_2_button = radio_button(
+	image("./songs/images/play_medium_2_border.png", 0, 0, 1                     ),
+	image("./songs/images/play_medium_2_red.png"   , 0, 0, 1, song_2.start_func()),
+	image("./songs/images/play_medium_2_green.png" , 0, 0, 1, song_2.stop_func ())
+);
+
+const song_3_button = radio_button(
+	image("./songs/images/play_small_1_border.png", 0, 0, 1                     ),
+	image("./songs/images/play_small_1_red.png"   , 0, 0, 1, song_3.start_func()),
+	image("./songs/images/play_small_1_green.png" , 0, 0, 1, song_3.stop_func ())
+);
+
 const play_buttons = radio_buttons(
 	song_0_button,
-	song_1_button
+	song_1_button,
+	song_2_button,
+	song_3_button
 );
 
 const click = _ => {
