8598a5779348512e66c107f18560daa1df915ec1
David Turner <csusbdt@gmail.com>
2023-07-01 20:01:58 -1000

    X

--- docs/composer/index.js
+++ docs/composer/index.js
@@ -3,7 +3,7 @@ import xbutton from "../global/xbutton.js";
 const back_button     = xbutton("upper_left_blue");
 const audio_blue      = xbutton("upper_right_blue");
 const audio_yellow    = xbutton("upper_right_yellow");
-const template_button = xbutton("medium_yellow", 100, 500);
+//const template_button = xbutton("medium_yellow", 100, 500);
 const grid_button     = xbutton("medium_yellow", 480, 500);
 
 const click_page = _ => {
@@ -12,7 +12,7 @@ const click_page = _ => {
 		on_resize();
 	}
 	else if (click(back_button    )) run("./home/index.js");
-	else if (click(template_button)) run("./composer/template/index.js");
+//	else if (click(template_button)) run("./composer/template/index.js");
 	else if (click(grid_button    )) run("./composer/grid/index.js");
 };
 
@@ -20,7 +20,7 @@ const draw_page = _ => {
 	bg_green.draw();
 	draw(back_button);
 	window.stop_audio === null ? draw(audio_blue) : draw(audio_yellow);
-	draw(template_button);
+//	draw(template_button);
 	draw(grid_button);
 };
 
